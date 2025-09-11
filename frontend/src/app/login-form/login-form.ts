69076d579579c44c45a5182198c3e679381f1cd8
Harish <harish.16634@gmail.com>
2025-09-11 12:34:00 +0530

    added login functionality

---
+++ frontend/src/app/login-form/login-form.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Api } from '../core/services/api/api';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-form.html',
+  styleUrl: './login-form.css',
+})
+export class LoginForm {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+
+  constructor(private fb: FormBuilder, private api: Api) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      this.api.loginUser(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+        next: (res) => {
+          console.log('Result: ', res);
+        },
+        error: (err) => {
+          console.log('[ERROR]', err);
+          this.errorMessage = err.statusText;
+        },
+      });
+    }
+  }
+}
