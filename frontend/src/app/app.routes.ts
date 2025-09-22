a678900f1e81ad41f303dc2518aaffd051f4556e
Chaithanya <ivcr1905@gmail.com>
2025-09-22 12:11:39 +0530

    made frontend changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginForm } from './pages/login-form/login-form';
 import { EmployeeDashboard } from './pages/employee-dashboard/employee-dashboard';
 import { AuthGuard } from './core/guards/auth.guard';
 import { ManagerDashboard } from './pages/manager-dashboard/manager-dashboard';
+import { HrDashboard } from './pages/hr-dashboard/hr-dashboard';
+import { AddEmployee } from './components/add-employee/add-employee';
 
 export const routes: Routes = [
   {
@@ -17,6 +19,16 @@ export const routes: Routes = [
   {
     path: 'manager/:id',
     component: ManagerDashboard,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'hr/:id',
+    component: HrDashboard,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'hr/:id/add-employee',
+    component: AddEmployee,
     canActivate: [AuthGuard],
   },
 ];
---
+++ frontend/src/app/components/add-employee/add-employee.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee',
+  imports: [],
+  templateUrl: './add-employee.html',
+  styleUrl: './add-employee.css'
+})
+export class AddEmployee {
+
+}
